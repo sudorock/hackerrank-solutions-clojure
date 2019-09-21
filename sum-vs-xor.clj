eeee112d6f141064f65f12596fc73d257fe45694
Sunil KS <kslvsunil@gmail.com>
2019-09-21 16:00:44 +0530

    Update sum-vs-xor.clj

--- sum-vs-xor.clj
+++ sum-vs-xor.clj
@@ -1,1 +1,21 @@
+;; start solution ;;
 
+(defn sumXor [n]
+  (if (zero? n) 
+    1
+    (loop [zeros 0 curr n]
+      (println zeros curr)
+      (cond
+        (< curr 1) (biginteger (Math/pow 2 zeros)) 
+        (zero? (mod curr 2)) (recur (inc zeros) (quot curr 2))
+        :else (recur zeros (quot curr 2))))))
+
+;; end solution ;;
+
+(def fptr (get (System/getenv) "OUTPUT_PATH"))
+
+(def n (Long/parseLong (clojure.string/trim (read-line))))
+
+(def result (sumXor n))
+
+(spit fptr (str result "\n") :append true) 
